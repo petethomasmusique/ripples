e1b15feaa2c8da072b2c8983d4e38af5ac008b50
Peter Thomas <you@example.com>
2018-03-06 19:20:37 +0000

    pool now a class

--- pool.js
+++ pool.js
@@ -1,4 +1,6 @@
+// pool to class
 // TODO: key scaling, when higher shorter so the env isn't the same for everything
+// start() stop() to optimize synths
 // note on off on mouse down / mouse up
 // horizontal dragging
 // hold down mouse?
@@ -8,6 +10,9 @@
 // AUDIO
 var audioCtx = new (window.AudioContext || window.webkitAudioContext); // create context
 var reverb = new Reverb(audioCtx, 'sounds/minster1_000_ortf_48k.wav');
+var poolNode = document.getElementById('pond');
+var tones = [45, 48, 50, 52, 55, 57, 60, 62, 64, 67, 69];
+var pool = new Pool(audioCtx, reverb.reverb, poolNode, tones);
 
 // THE POND
 var pondObj = {
@@ -24,10 +29,10 @@ window.addEventListener('mousedown', () => pondObj.mouseDown = true);
 window.addEventListener('mouseup', () => pondObj.mouseDown = false);
 
 // CREATE
-createPond(pondObj);
-createSynths(pondObj, reverb.reverb);
-setSynthsTimbre(pondObj);
+// createPond(pondObj);
+// createSynths(pondObj, reverb.reverb);
+// setSynthsTimbre(pondObj);
 
 // HOUSEKEEPING
-setInterval(() => deleteRipples(), 1000);
+// setInterval(() => deleteRipples(), 1000);
 // TODO: turn off none playing synths...
